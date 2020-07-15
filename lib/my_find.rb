28c3063c24efe17c1f79c965672caf918d175e59
Katie Le <lethikatie@gmail.com>
2020-07-15 00:10:23 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,10 +1,9 @@
 def my_find(collection)
   i = 0
   while i < collection.length
-    if 
-  yield(collection[i])
-      return 
-  collection[i]
+    if yield(collection[i])
+      return collection[i]
+    end
     i = i + 1
   end
-end 
+end
